99452064a1f1a6d501b44b8396e2e179ba1e76d7
Ethan <ethan_rosenfeld@horacemann.org>
2025-04-11 15:59:44 -0400

    New method using Jefimenko–Feynman solution

--- Testing/indexing.jl
+++ Testing/indexing.jl
@@ -1,18 +1,34 @@
 const xs = [i for i in -5:0.5:5]; const ys = [i for i in -5:0.5:5]; const zs = [i for i in -5:0.5:5] # Spatial bounds
+using Base.Threads
 
-vector_field = [[x, y, z] for x in xs, y in ys, z in zs]
+points = [[x, y, z] for x in xs, y in ys, z in zs]
 
-function index(x, y, z)
-    x_pos = (x - xs[begin]) / (xs[end] - xs[begin])
-    y_pos = (y - ys[begin]) / (ys[end] - ys[begin])
-    z_pos = (z - zs[begin]) / (zs[end] - zs[begin])
-    idx_x = trunc(Int, x_pos * (length(xs) - 1) + 1)
-    idx_y = trunc(Int, y_pos * (length(xs) - 1) + 1)
-    idx_z = trunc(Int, z_pos * (length(xs) - 1) + 1)
-    println(vector_field[idx_x, idx_y, idx_z])
-    # return findall(i->i==[x, y, z], vector_field)
+function pos_to_idx(x, x_min, x_max, x_step)
+    if x < x_min || x > x_max; error("Position out of bounds"); end
+    index = (x - x_min)/x_step + 1
+    return trunc(Int, index)
 end
 
-index(-5, -1.5, 3.5)
+println(pos_to_idx(0, -5, 5, 0.5))
 
-# println(vector_field[1, end, end])
+vectors = vec([zeros(3) for x in xs, y in ys, z in zs])
+
+num_threads = 8
+len = length(vec(vector_field))
+split_len = ceil(len / num_threads)
+
+index_splits = append!([trunc(Int, n) for n in 1:split_len:len], len)
+
+positions = [[0, 1, 0], [0, -1, 0], [1, 0, 0], [-1, 0, 0]]
+
+@threads for i in 1:num_threads
+    start_idx = index_splits[i]
+    end_idx = index_splits[i + 1]
+    working_points = vec(points)[start_idx:end_idx]
+    for pos in positions
+        for j in eachindex(working_points)
+            point = working_points[j]
+            vectors[j + start_idx - 1] = pos - point / hypot((pos - point)...)
+        end
+    end
+end
